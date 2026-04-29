218d97ebdc43cb5cfd218e34c5236f69854f31c7
TuanNgo <bamsilly04@gmail.com>
2026-04-29 14:41:02 +0700

    add connect be for demo

--- backend/src/users/db-pool.ts
+++ backend/src/users/db-pool.ts
@@ -1,6 +1,16 @@
 import { Pool } from "pg";
 
 export function createPgPool() {
+  if (process.env.DATABASE_URL) {
+    return new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : false
+    });
+  }
+
   return new Pool({
     host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT || 5432),
